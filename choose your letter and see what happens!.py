e2c8812a3652c9a2d065a0d26438f890565c6476
Makki <34487148+mdmakki@users.noreply.github.com>
2018-06-26 11:06:47 -0400

    Add files via upload

---
+++ choose your letter and see what happens!.py
@@ -0,0 +1,32 @@
+import time 
+z = ("Y")
+g = ("N")
+print("Debugger is checking for any bugs... please wait.")
+print("Debugger is checking for any bugs... please wait.")
+print("Debugger is checking for any bugs... please wait ")
+print("No bugs found")
+
+for i in range(30):
+  print("Processing... please wait.")
+  time.sleep(0.25)
+
+print("Code is ready...")
+a = input("Please enter one of these letters: L, R or D ") 
+     
+n = ("L")
+m = ("R")
+z = ("D")
+if (a == n):
+ print("CONGRATULATIONS, YOU HAVE SUBMITTED THE LETTER L!!!")
+elif (a == m):
+  print("CONGRATULATIONS, YOU HAVE SUBMITTED THE LETTER R") 
+elif (a == z):
+   print("CONGRATULATIONS, YOU HAVE SUBMITTED THE LETTER D")
+whileloop = True
+c = (input("Thank you for being patient. Press Enter key if you want to proceed to a game."))
+
+for i in range(100):
+  print("WE HAVE BEEN ATTACKED BY A BUG!!! EMERGENCY SHUTDOWN!!!")
+  time.sleep(0.012)
+import sys
+sys.exit()
